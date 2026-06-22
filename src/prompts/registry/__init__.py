b2f12daeda9679c10e045a8913572eccc31bc4e7
LaySleek <aleisle@mail.ru>
2026-06-22 21:18:01 +0700

    chores: add get_prompt_registry to __init__

--- src/prompts/registry/__init__.py
+++ src/prompts/registry/__init__.py
@@ -1,5 +1,6 @@
-from .prompt_registry import PromptRegistry
+from .prompt_registry import PromptRegistry, get_prompt_registry
 
 __all__: list[str] = [
     "PromptRegistry",
+    "get_prompt_registry",
 ]
